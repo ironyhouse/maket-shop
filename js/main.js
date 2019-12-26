85b51cb12065dee8dd94d154a234c24c2e986d08
ironylait <ironylait@mail.ru>
2019-12-26 03:04:47 +0300

    feat: add mobile version

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
+/* Slider */
 let tBackgroundElem = document.querySelector(`.slider`);
 let images = [
-	`/img/slide2.jpg`,
   `./img/slide1.jpg`,
-  `./img/slide2.jpg`
+	`./img/1.svg`
 ]
 let i = 0;
 function changeBackgroundImage() {
 	if(i === images.length) {
   	i = 0;
   }
-	tBackgroundElem.style.backgroundImage = `url(${images[i]})`;
+	tBackgroundElem.style.background = `linear-gradient(0deg, rgba(52, 54, 59, 0.6), rgba(52, 54, 59, 0.6)), url(${images[i]}) no-repeat`;
   i++;
 };
+let interval = setInterval(changeBackgroundImage, 5000)
 
-let interval = setInterval(changeBackgroundImage, 3000)
+
+
+/* Drop Button */
+function showSearch() {
+  document.querySelector(`.button_search`).style.display = 'block';
+}
+function showMenu() {
+  document.querySelector(`.button_menu`).style.display = 'block';
+}
+function hideElement() {
+  document.querySelector(`.button_menu`).style.display = 'none';
+  document.querySelector(`.button_search`).style.display = 'none';
+}
