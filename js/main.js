c9fa2e300bf8ae377df8ca8e4a1c9172c9999722
ironylait <ironylait@mail.ru>
2020-01-12 01:42:09 +0300

    fix: change img

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 /* Slider */
-let tBackgroundElem = document.querySelector(`.slider`);
-let images = [
-  `./img/slide1.jpg`,
-	`./img/1.svg`
-]
-let i = 0;
-function changeBackgroundImage() {
-	if(i === images.length) {
-  	i = 0;
-  }
-	tBackgroundElem.style.background = `linear-gradient(0deg, rgba(52, 54, 59, 0.6), rgba(52, 54, 59, 0.6)), url(${images[i]}) no-repeat`;
-  i++;
-};
-let interval = setInterval(changeBackgroundImage, 5000)
+// let tBackgroundElem = document.querySelector(`.slider`);
+// let images = [
+//   `./img/slide1.jpg`,
+// 	`./img/1.svg`
+// ]
+// let i = 0;
+// function changeBackgroundImage() {
+// 	if(i === images.length) {
+//   	i = 0;
+//   }
+// 	tBackgroundElem.style.background = `linear-gradient(0deg, rgba(52, 54, 59, 0.6), rgba(52, 54, 59, 0.6)), url(${images[i]}) no-repeat`;
+//   i++;
+// };
+// let interval = setInterval(changeBackgroundImage, 5000)
 
 
 
